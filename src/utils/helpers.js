8cfa233f5b44a8f2bdb39c60306c1d78af9509d7
EkhtiarUddin <uddinikhtiar6@gmail.com>
2025-11-06 01:07:59 +0600

    verifications

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,45 +1,36 @@
-// src/utils/helpers.js
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
-
-// Password hashing
 const hashPassword = async (password) => {
   const saltRounds = 12;
   return await bcrypt.hash(password, saltRounds);
 };
 
-// Password verification
 const verifyPassword = async (password, hashedPassword) => {
   return await bcrypt.compare(password, hashedPassword);
 };
 
-// JWT token generation
 const generateToken = (userId) => {
   return jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
 
-// Generate random string
 const generateRandomString = (length = 10) => {
   return crypto.randomBytes(length).toString('hex');
 };
 
-// Sanitize user object
 const sanitizeUser = (user) => {
   const { password, ...sanitizedUser } = user;
   return sanitizedUser;
 };
 
-// Pagination helper
 const getPagination = (page, size) => {
   const limit = size ? +size : 10;
   const offset = page ? (page - 1) * limit : 0;
   return { limit, offset };
 };
 
-// Search helper
 const buildSearchConditions = (search, searchFields) => {
   if (!search) return {};
   
